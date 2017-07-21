68ce5108263dca8eae276619578e3ac66f9c1cc6
Duong Van Hop <yangahe@gmail.com>
2017-07-21 15:32:06 +0900

    [Add] Add case to handle error based on server status

--- APIUtility/ApiRequest.swift
+++ APIUtility/ApiRequest.swift
@@ -11,6 +11,7 @@ import Himotoki
 
 public protocol APIRequest {
     associatedtype Response
+    associatedtype Error
     var baseURL: String { get }
     var method: Alamofire.HTTPMethod { get }
     var path: String { get }
@@ -18,10 +19,23 @@ public protocol APIRequest {
     var headerFields: [String : String] { get }
 
     func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Self.Response
+    func errorResponse(form object: Any, urlResponse: HTTPURLResponse) throws -> Self.Error
 }
 
 public extension APIRequest where Response: Decodable {
     public func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
         return try Response.decodeValue(object)
     }
+}
+
+public extension APIRequest where Error: Decodable {
+    public func errorResponse(from object: Any, urlResponse: HTTPURLResponse) throws -> Error {
+        return try Error.decodeValue(object)
+    }
+}
+
+public enum ResponseHeader: Int {
+    case active
+    case forceUpdate
+    case maintenance
 }
--- APIUtility/ApiService.swift
+++ APIUtility/ApiService.swift
@@ -38,4 +38,61 @@ open class ApiService {
                 return try item.response(from: json, urlResponse: response)
             })
     }
+
+    open func fetchWithErr<T: APIRequest>(_ item: T) -> Observable<(T.Error?, T.Response?)> {
+        return Observable.create { observer -> Disposable in
+            var requestParams = ""
+            if let params = item.parameters {
+                requestParams = params.sorted { $0.0 < $1.0 }
+                    .map { "\($0)=\($1)" }
+                    .joined(separator: "&")
+            }
+
+            guard let url = URL(string: item.baseURL + item.path + requestParams) else {
+                return Disposables.create()
+            }
+
+            return RxAlamofire.requestJSON(item.method, url, headers: item.headerFields)
+                .subscribe(
+                    onNext: { [weak self] (response, value) in
+                        guard let weakSelf = self else { return }
+                        // Check the response code if needed
+                        if response.statusCode == 200 {
+                            for header in response.allHeaderFields {
+                                let responseHeader = weakSelf.headerType(header.key.description)
+                                switch responseHeader {
+                                case .maintenance, .forceUpdate:
+                                    guard let error = try? item.errorResponse(form: value, urlResponse: response) else { return }
+                                    observer.on(.next((error, nil)))
+                                default:
+                                    guard let result = try? item.response(from: value, urlResponse: response) else { return }
+                                    observer.on(.next((nil, result)))
+                                }
+                            }
+                        } else {
+                            guard let error = try? item.errorResponse(form: value, urlResponse: response) else { return }
+                            observer.on(.next((error, nil)))
+                        }
+
+                    },
+                    onError: { (error) in
+                        observer.on(.error(error))
+                },
+                    onCompleted: {
+                        observer.on(.completed)
+                })
+            
+        }
+    }
+
+    private func headerType(_ header: String) -> ResponseHeader {
+        switch header {
+        case "X-Force-Update":
+            return .forceUpdate
+        case "X-Force-Maintenance":
+            return .maintenance
+        default:
+            return .active
+        }
+    }
 }
